845143543613c8aba1c82119488bed2e9539df4e
tolaoguntunde <50242126+tolaoguntunde@users.noreply.github.com>
2021-11-24 21:52:04 -0500

    update assignmeny

--- app.js
+++ app.js
@@ -1,139 +1,116 @@
-// // let randomNumber = Math.random();
-// // randomNumer = randomNumber * 6;
-// // randomNumber =Math.floor(randomNumber) + 1;
-// // console.log(randomNumber)
-
-// // prompt("What is your name");
-// // prompt("what is your friends name");
-
-// // let friendshipScore = Math.random()* 100;
-// // friendshipScore = Math.floor(friendshipScore) + 1;
-// // // alert(`Your frienhip Score is ${friendshipScore}%`);
-// // if (friendshipScore >=70) {
-// //     console.log(`Your friendship score is above 70 score: ${friendshipScore}`)
-// // }
-
-
-// // let randomNum =Math.random();
-// // randomNum = randomNum * 100 ;
-// // randomNum = Math.floor(randomNum) + 1;
-
-// // if (randomNum % 2  === 0) {
-// //     console.log(`Your number ${randomNum} is even`)
-// // }
 
 // // =============================Assignment==================//
 
 // // Question 1
 
-// // let a = prompt("Enter first argument");
-// // let b = prompt("Enter second argument");
-// // if (a !== b) {
-// //     alert("The values are not equal")
-// // } else {
-// //     alert("They are equal")
-// // }
+let a = prompt("Enter first argument");
+let b = prompt("Enter second argument");
+if (a !== b) {
+    alert("The values are not equal")
+} else {
+    alert("They are equal")
+}
 
-// // let speedValue = prompt("Enter your speed");
-// // let speedLimitValue = prompt("Enter your speed limit");
+let speedValue = prompt("Enter your speed");
+let speedLimitValue = prompt("Enter your speed limit");
 
 // // // Question 2
 
-// // function speedChecker(speed, speedLimit) {
-// //     if (speed >= speedLimit) {
-// //         alert("Yes, you are speeding")
-// //     } else {
-// //         alert("No you are not speeding.What's wrong with you")
-// //     }
-// // }
+function speedChecker(speed, speedLimit) {
+    if (speed >= speedLimit) {
+        alert("Yes, you are speeding")
+    } else {
+        alert("No you are not speeding.What's wrong with you")
+    }
+}
 
-// // speedChecker(speedValue, speedLimitValue)
+speedChecker(speedValue, speedLimitValue)
 
 // // // Question 3
 
-// // function countSpoons(){
-// //     let count = 10;
-// //     let spoons = 11;
-// //     if (count === spoons) {
-// //         alert("We have spoons for everyone")
-// //     }
+function countSpoons(){
+    let count = 10;
+    let spoons = 11;
+    if (count === spoons) {
+        alert("We have spoons for everyone")
+    }
     
-// //     else if (count < spoons) {
-// //         alert("Bring more spoons please")
-// //     } else {
-// //         alert("We have a problem")
-// //     }
-// // }
+    else if (count < spoons) {
+        alert("Bring more spoons please")
+    } else {
+        alert("We have a problem")
+    }
+}
 
-// // countSpoons()
+countSpoons()
 
 // // // Question 4
 
-// // hourValue=prompt("Enter number of hours:");
-// // function officeHours(hour){
-// //     if (hour < 10 || hour > 10 ) {
-// //      alert("The office is closed")   
-// //     }else{
-// //         alert("Open for business")
-// //     }
-// // }
+hourValue=prompt("Enter number of hours:");
+function officeHours(hour){
+    if (hour < 10 || hour > 10 ) {
+     alert("The office is closed")   
+    }else{
+        alert("Open for business")
+    }
+}
 
-// // officeHours(hourValue)
+officeHours(hourValue)
 
 // // // Question 5
 
-// // let hourOpenValue=prompt("Enter hours of operation");
-// // checkWeekend = true ;
-// // function isOfficeOpen(hour,isWeekend){
-// // if (hour < 10 || hour > 18 || isWeekend === true) {
-// //     alert("The office is closed")
-// // }
-// // }
+let hourOpenValue=prompt("Enter hours of operation");
+checkWeekend = true ;
+function isOfficeOpen(hour,isWeekend){
+if (hour < 10 || hour > 18 || isWeekend === true) {
+    alert("The office is closed")
+}
+}
 
-// // isOfficeOpen(hourOpenValue,checkWeekend)
+isOfficeOpen(hourOpenValue,checkWeekend)
 
 // // // Question 6
 
-// // function leavingTime(){
-// //     let hour = 12;
-// //     let minute = 30;
-// //     if (hour === 12 && minute === 30) {
-// //         alert("We are leaving at 12:30"); 
-// //     }else{
-// //         alert("The time is")
-// //     }
-// // }
-// // leavingTime()
+function leavingTime(){
+    let hour = 12;
+    let minute = 30;
+    if (hour === 12 && minute === 30) {
+        alert("We are leaving at 12:30"); 
+    }else{
+        alert("The time is")
+    }
+}
+leavingTime()
 
 
 // // //Question 7
 
-// // function pickBook(){
-// //     let book = "maths";
-// //     if (book === "history") {
-// //         alert("History Book");
-// //     }
-// //     else if (book === "maths") {
-// //         alert("Maths Book");
-// //     }else if (book === "economics") {
-// //         alert("Economics Book");
-// //     }else{
-// //         alert("Unknown book")
-// //     }
-// // }
-// // pickBook()
+function pickBook(){
+    let book = "maths";
+    if (book === "history") {
+        alert("History Book");
+    }
+    else if (book === "maths") {
+        alert("Maths Book");
+    }else if (book === "economics") {
+        alert("Economics Book");
+    }else{
+        alert("Unknown book")
+    }
+}
+pickBook()
 
 
 // // // Question 8
 
-// // ageValue = prompt("Enter you age:");
-// // function checkEligibility(age){
-// // if (age > 18) {
-// //     alert("Qualifies for driving")  
-// // }else{
-// //     alert("Does not qualify for driving")
-// // }
-// // }
+ageValue = prompt("Enter you age:");
+function checkEligibility(age){
+if (age > 18) {
+    alert("Qualifies for driving")  
+}else{
+    alert("Does not qualify for driving")
+}
+}
 
 // // checkEligibility(ageValue)
 
@@ -200,7 +177,7 @@
 // let myFruit=fruit.reverse();
 // console.log(myFruit.join(''));
 
-
+//-----------------Assignment(Nov 20)---------------------------
 function reverse(str){
   const fruit =str.split('');
   let myFruit=fruit.reverse();
